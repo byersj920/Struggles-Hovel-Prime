38ad7a56c84fa0c631e1d7d2cbd06272b3b42fec
Jonathan <byersj920@gmail.com>
2024-09-30 11:21:38 -0500

    Card page does everything but remove now

--- src/Components/CardDisplay.js
+++ src/Components/CardDisplay.js
@@ -20,7 +20,7 @@ const CardDisplay = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://cwischmeyer.synology.me:9696/api/cards');
+        const response = await axios.get('https://struggleshovel.cwischmeyer.synology.me/api/cards');
         setCardData(response.data);
       } catch (error) {
         console.error('Error fetching card data:', error);
@@ -68,13 +68,13 @@ const CardDisplay = () => {
     }
 
     try {
-      await axios.post('http://cwischmeyer.synology.me:9696/api/cards/add', null, {
+      await axios.post('https://struggleshovel.cwischmeyer.synology.me/api/cards/add', null, {
         params: {
           cardId: encodeURIComponent(cardId),
           username: encodeURIComponent(discordName)
         }
       });
-      const response = await axios.get('http://cwischmeyer.synology.me:9696/api/cards');
+      const response = await axios.get('https://struggleshovel.cwischmeyer.synology.me/api/cards');
       setCardData(response.data);
     } catch (error) {
       console.error('Error adding card:', error);
@@ -88,13 +88,13 @@ const CardDisplay = () => {
     }
 
     try {
-      await axios.post('http://cwischmeyer.synology.me:9696/api/cards/remove', null, {
+      await axios.post('https://struggleshovel.cwischmeyer.synology.me/cards/remove', null, {
         params: {
           cardId: encodeURIComponent(cardId),
           username: encodeURIComponent(discordName)
         }
       });
-      const response = await axios.get('http://cwischmeyer.synology.me:9696/api/cards');
+      const response = await axios.get('https://struggleshovel.cwischmeyer.synology.me/api/cards');
       setCardData(response.data);
     } catch (error) {
       console.error('Error removing card:', error);
